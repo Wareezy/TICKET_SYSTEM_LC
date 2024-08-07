01642571c2749034a6ffbd4b93d266aab36f09c3
Brandon <brandonroulstone71@gmail.com>
2024-08-07 13:36:51 +0200

    CRUD COMPLETED

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,18 +1,42 @@
-import { getUsers } from "../Model/UserModel.js";
+import { getUsers, getUser, addUser, updateUser } from "../Model/UserModel.js";
 
 export default {
     GET_ALL_USERS: async (req, res) => {
         try {
-            res.send(await getUsers())
+            res.send(await getUsers());
         } catch (error) {
-            res.send({ error: error.message, serverRes: 500 })
+            res.send({ error: error.message, serverRes: 500 });
         }
     },
     GET_A_USER: async (req, res) => {
-        try {
-            
+        try { 
+            res.send(await getUser(+req.params.id));
         } catch (error) {
-            res.send({ error: error.message, serverRes: 500 })
+            res.send({ error: error.message, serverRes: 500 });
+        }
+    },
+    ADD_USER: async (req, res) => {
+        try {
+            const { user_email, user_password, user_name, user_surname } = req.body;
+            await addUser(user_email, user_password, user_name, user_surname);
+            res.send(await getUsers());
+        } catch (error) {
+            res.send({ error: error.message, serverRes: 500 });
+        }
+    },
+    UPDATE_USER: async (req, res) => {
+        try {
+            const [user] = getUser(+req.params.id);
+            const { user_email, user_password, user_name, user_surname } = req.body;
+            user_email ? user_email : { user_email } = user;
+            user_password ? user_password : { user_password } = user;
+            user_name ? user_name : { user_name } = user;
+            user_surname ? user_surname : { user_surname } = user;
+            
+            await updateUser(ser_email, user_password, user_name, user_surname, +req.params.id);
+            res.send(await getUsers());
+        } catch (error) {
+            res.send({ error: error.message, serverRes: 500 });
         }
     }
 }
--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import controller from '../Controller/UserController.js'
 
-const router = Router()
-
+const router = Router();
 
 router.route('/')
     .get(controller.GET_ALL_USERS)
+    .post(controller.ADD_USER)
 
+router.route('/:id')
+    .get(controller.GET_A_USER)
+    .patch(controller.UPDATE_USER)
 
-export default router
+export default router;
