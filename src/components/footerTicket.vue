c211859d9849281073c669f202974304ed44ca04
Warren <warrenjaftha16@gmail.com>
2024-08-08 08:42:01 +0200

    footer

--- src/components/footerTicket.vue
+++ src/components/footerTicket.vue
@@ -3,7 +3,7 @@
    <div class="container">
      <div class="row">
        <p class="lead">
-         OTAKU OASIS &copy;
+         StudioTicketFlow &copy;
          <span>{{currYear}}</span>
        </p>
      </div>
