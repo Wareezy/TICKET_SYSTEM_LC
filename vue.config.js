774a764ac1cea8139e56af5cd003f0dfade7f5b9
Brandon <brandonroulstone71@gmail.com>
2024-09-10 12:12:43 +0200

    Data properties were incorrect and not sending to the database issue fixed

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+const fs = require('fs')
+const path = require('path');
+module.exports = {
+  devServer: {
+    host: '192.168.8.215',
+    port: 8080,
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, '../../../Intern-38/ssl/local.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, '../../../Intern-38/ssl/local.crt')),
+    },
+  },
+}
 module.exports = defineConfig({
   transpileDependencies: true
 })
